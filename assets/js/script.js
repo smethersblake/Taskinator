f9a62c64593454769babc79fa78bbbd62c3a0bed
Blake Smethers <smethersblake@gmail.com>
2022-03-27 17:44:58 -0500

    add submit listener to form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
-var buttonEl = document.querySelector('#save-task');
+var formE1 = document.querySelector('#task-form');
 var tasksToDoE1 = document.querySelector('#tasks-to-do');
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = 'This is a new task';
     tasksToDoE1.appendChild(listItemEl);
 }
-buttonEl.addEventListener("click", createTaskHandler);
+formE1.addEventListener("submit", createTaskHandler);
