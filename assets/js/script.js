1c1252562215ee252b13f38f5ed6932d294be29f
Blake Smethers <smethersblake@gmail.com>
2022-03-28 18:19:47 -0500

    add submit features to display input field as task

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,20 @@ var formE1 = document.querySelector('#task-form');
 var tasksToDoE1 = document.querySelector('#tasks-to-do');
 var createTaskHandler = function(event) {
     event.preventDefault();
-    
+    var taskNameInput = document.querySelector("input[name = 'task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // create list item
+    console.log(taskTypeInput);
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = 'This is a new task';
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML contente to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+    // listItemEl.textContent = taskNameInput;
     tasksToDoE1.appendChild(listItemEl);
 }
 formE1.addEventListener("submit", createTaskHandler);
